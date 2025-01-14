12d3a24493c3a22ca89b213cd9e02eb8ee1aed41
bryceberczik <berczikbryce@gmail.com>
2025-01-13 20:34:28 -0600

    completed user model

---
+++ server/src/models/user.ts
@@ -0,0 +1,100 @@
+import { DataTypes, Sequelize, Model, Optional } from "sequelize";
+import bcrypt from "bcrypt";
+
+interface IUser {
+  id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+}
+
+interface ICreateUser extends Optional<IUser, "id"> {}
+
+export class User extends Model<IUser, ICreateUser> implements IUser {
+  public id!: string;
+  public firstName!: string;
+  public lastName!: string;
+  public email!: string;
+  public password!: string;
+
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+
+  public async setPassword(password: string) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(password, saltRounds);
+  }
+}
+
+export function UserFactory(sequelize: Sequelize): typeof User {
+  User.init(
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+      },
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: { msg: "First name is required" },
+          notEmpty: { msg: "First name cannot be empty" },
+          len: {
+            args: [0, 20],
+            msg: "Username must be between 0 and 20 characters long",
+          },
+        },
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: { msg: "Last name is required" },
+          notEmpty: { msg: "Last name cannot be empty" },
+          len: {
+            args: [0, 20],
+            msg: "Username must be between 0 and 20 characters long",
+          },
+        },
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          notNull: { msg: "Email is required" },
+          isEmail: { msg: "Email must be valid" },
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: { msg: "Password is required" },
+          len: {
+            args: [8, 128],
+            msg: "Password must be between 8 and 128 characters long",
+          },
+        },
+      },
+    },
+    {
+      tableName: "users",
+      sequelize,
+      hooks: {
+        beforeCreate: async (user: User) => {
+          await user.setPassword(user.password);
+        },
+        beforeUpdate: async (user: User) => {
+          if (user.changed("password")) {
+            await user.setPassword(user.password);
+          }
+        },
+      },
+    }
+  );
+
+  return User;
+}
