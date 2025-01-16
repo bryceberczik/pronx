f4dc71c29e7e661864d1d365a457f2a75add3f61
bryceberczik <berczikbryce@gmail.com>
2025-01-15 18:33:48 -0600

    tested frontend / working out wirefrime

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
-
-function App() {
+import { Outlet } from "react-router-dom";
+const App = () => {
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <p>Start prompting (or editing) to see magic happen :)</p>
+    <div>
+      <main>
+        <Outlet />
+      </main>
     </div>
   );
-}
+};
 
 export default App;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,29 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from './App.tsx';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+import ErrorPage from "./pages/ErrorPage.tsx";
+import Home from "./pages/Home.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: (
+            <Home />
+        ),
+      },
+    ],
+  },
+]);
+
+const rootElement = document.getElementById("root");
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(<RouterProvider router={router} />);
+}
