b99a900b9ffc7e637a671540d0ad22006cbf10cf
bryceberczik <berczikbryce@gmail.com>
2025-01-16 11:16:00 -0600

    added code for auth

---
+++ client/src/utils/auth.ts
@@ -0,0 +1,58 @@
+import { jwtDecode } from "jwt-decode";
+
+class AuthService {
+  loggedIn() {
+    const token = this.getToken();
+    return token && !this.isTokenExpired(token);
+  }
+
+  getToken(): string {
+    return localStorage.getItem("id_token") || "";
+  }
+
+  isTokenExpired(token: string): boolean {
+    try {
+      const decoded: { exp: number } = jwtDecode(token);
+      const currentTime = Date.now() / 1000; // Current time in seconds
+      return decoded.exp < currentTime;
+    } catch (error) {
+      console.error("Failed to decode token:", error);
+      return true; // Treat as expired if decoding fails
+    }
+  }
+
+  getProfile() {
+    const token = this.getToken();
+    if (token && !this.isTokenExpired(token)) {
+      try {
+        const decoded = jwtDecode<{ id: string; email: string, username: string }>(token);
+        return decoded;
+      } catch (error) {
+        console.error("Failed to decode token:", error);
+        return null;
+      }
+    }
+    return null;
+  }
+
+  login(idToken: string) {
+    localStorage.setItem("id_token", idToken);
+    window.location.assign("/");
+  }
+
+  logout() {
+    localStorage.removeItem("id_token");
+    window.location.assign("/");
+  }
+
+  handleTokenExpiration() {
+    const token = this.getToken();
+    if (token && this.isTokenExpired(token)) {
+      this.logout();
+    }
+  }
+}
+
+const authService = new AuthService();
+
+export default authService;
